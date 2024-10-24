4d7709325d9067dc94c2db6b05aa66267374c78b
Zx113141 <1184157652@qq.com>
2024-10-24 14:29:28 +0800

    feat: fullscreen loading when load nuxt pages

--- app/beetr/app.vue
+++ app/beetr/app.vue
@@ -1,7 +1,26 @@
 <template>
-  <NuxtPage />
+  <div>
+    <ModuleFullscreenLoading v-if="isFullLoading" />
+    <NuxtPage />
+  </div>
+
 </template>
 <script setup lang="ts">
+import ModuleFullscreenLoading from './components/module-fullscreen-loading.vue';
+const nuxtApp = useNuxtApp()
+
+// 是否首次加载
+const isFullLoading = ref(true)
+
+nuxtApp.hook('page:start', () => {
+  isFullLoading.value = true
+})
+
+nuxtApp.hook('page:finish', () => {
+  setTimeout(() => {
+    isFullLoading.value = false
+  }, 2000)
+})
 // const resizeWrapHeight = () => {
 //   let vh = window.innerHeight * 0.01
 //   document.documentElement.style.setProperty('--vh', `${vh}px`)
